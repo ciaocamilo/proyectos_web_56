cf195d1d2a1aea6cd3440be1fe6ea1c442f7b6c9
Camilo A <ing_camiloacg@hotmail.com>
2021-09-23 15:31:58 -0500

    Agrega detalles finales a la versión 1 de la tienda de ropa

--- TIENDA DE ROPA/Version 1/scripts/producto.js
+++ TIENDA DE ROPA/Version 1/scripts/producto.js
@@ -1,12 +1,18 @@
 let carrito = [];
 let total = 0;
 
-function agregarAlCarrito(nombre_producto, precio_producto) {
+function agregarAlCarrito(nombre_producto, precio_producto, cantidad_producto) {
     // alert(nombre_producto + ' ' + precio_producto);
-    let producto = { nombre: nombre_producto, precio: precio_producto };
+    let cantidad = document.getElementById(cantidad_producto).value;
+    let producto = { nombre: nombre_producto, precio: precio_producto, cantidad: cantidad };
     carrito.push(producto);
     console.log(carrito);
-    console.log(carrito[1].nombre);
-    total = total + precio_producto;
+    // console.log(carrito[1].nombre);
+    total = total + (precio_producto * cantidad);
     total_carrito.innerText = '$' + total;
+    Swal.fire(
+        'Producto agregado',
+        'Se ha agregado ' + nombre_producto + ' al carrito de compras',
+        'success'
+        );
 }
