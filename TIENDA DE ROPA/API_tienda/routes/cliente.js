13cbf1c4d8bc1411f992816c27b8e87b98d133c8
Camilo A <ing_camiloacg@hotmail.com>
2021-10-07 19:08:34 -0500

    Agrega crud de cliente en backend

--- TIENDA DE ROPA/API_tienda/routes/cliente.js
+++ TIENDA DE ROPA/API_tienda/routes/cliente.js
@@ -19,6 +19,59 @@ router.post('/nuevo-cliente', async(req, res) => {
     }
 });
 
+// Ruta para buscar cliente
+router.get('/cliente/:identificacion', async(req, res) => {
+    const identificacion = req.params.identificacion;
+    try {
+        const clienteDB = await Cliente.findOne({identificacion});
+        res.json(clienteDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrió un error al buscar cliente',
+            error
+        })
+    }
+});
+
+// Ruta para actualizar cliente
+router.put('/actualizar-cliente/:id', async(req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+    try {
+        const clienteDB = await Cliente.findByIdAndUpdate(
+            _id,
+            body,
+            {new: true}
+        );
+        res.json(clienteDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrió un error al actualizar el cliente',
+            error
+        })
+    }
+});
+
+// Ruta para eliminar cliente
+router.delete('/eliminar-cliente/:id', async(req, res) => {
+    const _id = req.params.id;
+    try {
+        const clienteDB = await Cliente.findByIdAndDelete({_id});
+        if(!clienteDB){
+            return res.status(400).json({
+                mensaje: 'No se encontró el cliente',
+                error
+            })
+        }
+        res.json(clienteDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrió un error al eliminar el cliente',
+            error
+        })
+    }
+});
+
 
 //Exportar configuración
 module.exports = router;
